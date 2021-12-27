8271d0e1fcf3adbc68a6f8da1abf0316bdddbcc1
Igor Salviatto <igorschsal@gmail.com>
2021-12-27 17:55:37 -0300

    fill width  image

--- nextjs-blog/components/NavBar/index.jsx
+++ nextjs-blog/components/NavBar/index.jsx
@@ -5,7 +5,7 @@ function NavBar() {
     return (
         <div className='navbar'>
             <Image src="/../public/adoteimg.png"
-                alt="adote" layout='fill'
+                alt="adote" height={300} width={1000}
             />
             <div>
                 <a className='redirect'>&bull; Home</a>
@@ -15,15 +15,19 @@ function NavBar() {
             <style jsx>{`
         .navbar{
         width: 100%;
+        height: auto;
 }
         .navbar, img{
+        height: auto;
         background-position: center;
         background-repeat: no-repeat;
         background-size: cover;
+        margin: 0;
         }
         .redirect{
         margin: 10%;
 }
+        
       `}</style>
         </ div>
     )
